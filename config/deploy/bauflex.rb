7c585e0c389d8745cd67208b8a50b4e997e86124
Denis Stepanov <denstepa@gmail.com>
2015-11-27 10:40:08 +0100

    prepare bauflex config

--- config/deploy/bauflex.rb
+++ config/deploy/bauflex.rb
@@ -12,12 +12,12 @@ set :server_name, '178.79.172.224'
 server '178.79.172.224', user: 'deploy', roles: %w{web app db}, primary: true
 
 set :stage, :bauflex
-set :branch, "master"
+set :branch, 'bauflex'
 set :deploy_to, "/home/#{fetch(:deploy_user)}/apps/#{fetch(:full_app_name)}"
 
 set :rails_env, :production
 
 set :enable_ssl, false
-set :nginx_use_ssl, true
+set :nginx_use_ssl, false
 
 set :nginx_server_name, 'x.bauflex.ru'
