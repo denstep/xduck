9278d99a96b54c68aa18cccff419ccfa17b7c6ad
Denis Stepanov <denstepa@gmail.com>
2015-12-16 11:23:53 +0100

    bauflex deploy test

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   #attr_accessible :email, :password, :roles, :role_ids
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, # :ldap_authenticatable, #:registerable,
+  devise :database_authenticatable, :ldap_authenticatable, #:registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   def to_s
